472434a7380b0aa9d038a7ee7cd2705948765b8e
André <cazetto.andre@gmail.com>
2021-11-08 21:35:28 -0300

    feat: install and configure aws amplify

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,13 +2,17 @@ import React, { FC } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core/styles';
 import { Provider } from 'react-redux';
+import Amplify from 'aws-amplify';
 import { store } from './store';
 import { LoginPage } from 'features/Login/LoginPage';
 import { DashboardPage } from 'features/Dashboard/DashboardPage';
 import { theme } from './theme';
 import { Text } from 'components/Text/Text';
+import { amplifyConfig } from 'config/amplify';
 
 export const TEXT_SITE_NAME = 'Arpex';
+
+Amplify.configure(amplifyConfig);
 
 export const App: FC = () => {
   return (
---
+++ src/config/amplify.ts
@@ -0,0 +1,6 @@
+export const amplifyConfig = {
+  aws_project_region: 'us-east-1',
+  aws_cognito_region: 'us-east-1',
+  aws_user_pools_id: 'us-east-1_QiNJIIQMJ',
+  aws_user_pools_web_client_id: '6egc0jq2hd4js3oqpehf6hkmlg',
+};
