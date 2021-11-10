e9cbafe4497fcad5f737cd16ead88cda855e6331
André <cazetto.andre@gmail.com>
2021-11-10 15:56:06 -0300

    fix: apply redux provider to login test

--- src/features/Login/LoginPage.test.tsx
+++ src/features/Login/LoginPage.test.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { render, screen } from '@testing-library/react';
 import { LoginPage } from './LoginPage';
 
+import { store } from 'app/store';
 import { TEXT_PAGE_TITLE } from './LoginPage';
 
 test('renders page', () => {
-  render(<LoginPage />);
+  render(
+    <Provider store={store}>
+      <LoginPage />
+    </Provider>
+  );
   const pageTitle = screen.getByText(TEXT_PAGE_TITLE);
   expect(pageTitle).toBeInTheDocument();
 });
